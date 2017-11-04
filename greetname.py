0952709bc5ee9deb2135777fce0a1e8417b962c4
ghufranmalic <30701224+ghufranmalic@users.noreply.github.com>
2017-11-04 12:47:36 +0500

    FirstApp

---
+++ greetname.py
@@ -0,0 +1,12 @@
+# This program says hello and ask for name
+
+print ('Hello There')
+print ('What is your name?') # Asks for name
+myName = input()
+print ('Its good to meet you ' + myName)
+print ('The lenght of your name is:')
+print (len (myName))
+
+print ('What is your age?') # Asks for age
+myAge = input()
+print ('You will ' + str(int(myAge) + 1) + ' next year')
